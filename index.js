715f85fa216e46e176ee12b796eaaa2362cb0442
Tsehaye <tsehayegh@gmail.com>
2018-02-14 08:49:38 -0500

    added browser compatibility

--- index.js
+++ index.js
@@ -296,4 +296,17 @@ function clearRoute(){
   }
 }
 
+//Browser compatibility
+function browserUpdateNotify(){
+  let $buoop = {notify:{i:-5,f:-4,o:-4,s:-2,c:-4},insecure:true,api:5}; 
+function $buo_f(){ 
+ var e = document.createElement("script"); 
+ e.src = "//browser-update.org/update.min.js"; 
+ document.body.appendChild(e);
+};
+try {document.addEventListener("DOMContentLoaded", $buo_f,false)}
+catch(e){window.attachEvent("onload", $buo_f)}
+
+}
+
 
